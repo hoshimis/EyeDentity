704a42188945157572bf48be201d9633b69f5144
hosimi <62686198+hoshimis@users.noreply.github.com>
2023-11-11 06:55:10 +0900

    rename:🚚 raneme image to images

--- backend/python/video_to_images.py
+++ backend/python/video_to_images.py
@@ -1,8 +1,8 @@
 import cv2
 import os
+import sys
 
-
-def split_video_to_images(input_video, output_dir, num_frames=30):
+def split_video_to_images(input_video, output_dir, file_name, num_frames=30):
     cap = cv2.VideoCapture(input_video)
     frame_count = 0
 
@@ -16,14 +16,17 @@ def split_video_to_images(input_video, output_dir, num_frames=30):
 
         frame_count += 1
         if frame_count % (cap.get(cv2.CAP_PROP_FRAME_COUNT) // num_frames) == 0:
-            image_filename = os.path.join(output_dir, f'frame_{frame_count:04d}.jpg')
+            image_filename = os.path.join(output_dir, f'{file_name}_{frame_count:04d}.jpg')
             cv2.imwrite(image_filename, frame)
 
     cap.release()
     cv2.destroyAllWindows()
 
 def main():
-  pass
+    # 渡された引数を受け取る
+    args = sys.argv
+    print(args)
+    split_video_to_images(args[1], args[2], args[3])
 
 
 if __name__ == '__main__':
