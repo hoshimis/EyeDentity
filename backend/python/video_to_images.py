3e0ee9f61d9ce038ca80a91b3d1b35a18255ccf3
hosimi <62686198+hoshimis@users.noreply.github.com>
2023-11-13 09:00:11 +0900

    feat:✨ feature video_to_face_image

--- backend/python/video_to_images.py
+++ backend/python/video_to_images.py
@@ -1,33 +1,79 @@
 import cv2
+import argparse
 import os
-import sys
 
-def split_video_to_images(input_video, output_dir, file_name, num_frames=30):
-    cap = cv2.VideoCapture(input_video)
-    frame_count = 0
+def detect_faces(video_path, save_image_path, face_id):
+    """
+    ビデオ内の顔を検出し、検出された顔の画像を保存します。
 
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    Args:
+        video_path (str): 動画ファイルへのパス
+        save_image_path (str): 顔画像を保存するディレクトリのパス
+        face_id (str): 顔を検出する対象者のID
 
-    while True:
+    Returns:
+        None
+    """
+    # classifierの読み込み
+    # Path の開始地点はBackendのディレクトリ
+    face_cascade = cv2.CascadeClassifier('./python/Cascades/haarcascade_frontalface_default.xml')
+    # 使用する動画ファイルのパスを指定
+    cap = cv2.VideoCapture(video_path)
+
+    # カメラの設定（サイズなど）
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+    # 保存先のディレクトリが存在しない場合は作成する
+    if not os.path.exists(f'{save_image_path}/{face_id}'):
+        os.makedirs(f'{save_image_path}/{face_id}')
+
+    for i in range(30):
         ret, frame = cap.read()
+
         if not ret:
+            print("Failed to capture video.")
             break
 
-        frame_count += 1
-        if frame_count % (cap.get(cv2.CAP_PROP_FRAME_COUNT) // num_frames) == 0:
-            image_filename = os.path.join(output_dir, f'{file_name}_{frame_count:04d}.jpg')
-            cv2.imwrite(image_filename, frame)
+        # グレースケールに変換
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # 顔の検出設定
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(20, 20)
+        )
+
+        for (x, y, w, h) in faces:
+            roi_gray = gray[y:y+h, x:x+w]
+
+            # 写真の保存場所
+            cv2.imwrite(f"{save_image_path}/{face_id}/{face_id}.{i}.jpg", gray[y:y+h, x:x+w])
+
+        k = cv2.waitKey(30) & 0xff
+        # ESC キーで終了
+        if k == 27:
+            break
+
 
     cap.release()
     cv2.destroyAllWindows()
-
-def main():
-    # 渡された引数を受け取る
-    args = sys.argv
-    print(args)
-    split_video_to_images(args[1], args[2], args[3])
-
+    print('Done')
 
 if __name__ == '__main__':
-  main()
+    parser = argparse.ArgumentParser(description='呼び出し元のNodeから引数を受け取る')
+    parser.add_argument('video_path', type=str, help='動画のパス')
+    parser.add_argument('sage_image_path', type=str, help='画像の保存先')
+    parser.add_argument('face_id', type=str, help='顔を検出する対象者のID')
+
+    # 引数の解析
+    args = parser.parse_args()
+
+    video_path = args.video_path
+    save_image_path = args.sage_image_path
+    face_id = args.face_id
+
+
+    detect_faces(video_path, save_image_path, face_id)
